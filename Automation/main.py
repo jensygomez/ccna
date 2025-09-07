f648b0d3c94214f10074c73908ccd6aabd245596
Jensy Gomez <contact@jensygomez.us>
2025-09-07 14:06:51 -0300

    ok

--- Automation/main.py
+++ Automation/main.py
@@ -1,20 +1,32 @@
 import telnetlib
+import time
 
 HOST = "192.168.1.2"
-USER = "admin"
 PASSWORD = "cisco"
 
 tn = telnetlib.Telnet(HOST)
-tn.read_until(b'Password:')
+
+# Login
+tn.read_until(b"Password:")
 tn.write(PASSWORD.encode('ascii') + b"\n")
 
+# Enable mode
 tn.write(b"enable\n")
+tn.read_until(b"Password:")
 tn.write(b"cisco\n")
+
+# Configuración
 tn.write(b"configure terminal\n")
 tn.write(b"hostname Sucursal_01\n")
 tn.write(b"end\n")
-tn.write(b"wr\n")
+tn.write(b"write memory\n")
 tn.write(b"\n")
 
-print(tn.read_all().decode('asscii'))
+# Dar tiempo a que el switch responda
+time.sleep(1)
+
+# Leer toda la salida
+output = tn.read_very_eager().decode('ascii')
+print(output)
+
 tn.close()
