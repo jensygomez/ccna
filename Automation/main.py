80995eb0669c4469ccac42e10ea4fa104546f3b2
Jensy Gomez <contact@jensygomez.us>
2025-09-07 13:49:03 -0300

    main

--- Automation/main.py
+++ Automation/main.py
@@ -1,1 +1,20 @@
-print("hola")
+import telnetlib
+
+HOST = "192.168.1.2"
+USER = "admin"
+PASSWORD = "cisco"
+
+tn = telnetlib.Telnet(HOST)
+tn.read_until(b'Password:')
+tn.write(PASSWORD.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"configure terminal\n")
+tn.write(b"hostname Sucursal_01\n")
+tn.write(b"end\n")
+tn.write(b"wr\n")
+tn.write(b"\n")
+
+print(tn.read_all().decode('asscii'))
+tn.close()
